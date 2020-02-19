c074261cdb4de3ffa3e7a4b453284769cfadbfcb
romncn <romncn@gmail.com>
2020-02-20 00:12:24 +0700

    update

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 
 const Navbar = (props) => {
-return(
-  <nav class="navbar navbar-light bg-light">
-    <span class="navbar-brand mb-0 h1">{props.children}</span>
-  </nav>
-)
+  return (
+    <nav class="navbar navbar-light bg-light">
+      <span class="navbar-brand mb-0 h1">{props.children}</span>
+    </nav>
+  )
 }
 
 export default Navbar;
